8469a5576715608e6ecfb1ad926245d2be3258c9
Christopher Grebs <cg@webshox.org>
2016-11-22 12:16:58 +0100

    Upgrade dependencies

--- setup.py
+++ setup.py
@@ -14,18 +14,17 @@ def read(*parts):
 
 test_requires = [
     # General test libraries
-    'tox==2.3.1',
-    'pytest==2.9.1',
-    'pytest-django==2.9.1',
+    'tox==2.5.0',
+    'pytest==3.0.4',
+    'pytest-django==3.1.1',
     'pytest-isort==0.1.0',
-    'pytest-asyncio==0.3.0',
 
     'tzlocal==1.2.2',
 
     # Pep8 and code quality checkers
-    'flake8==2.5.4',
+    'flake8==3.2.1',
     'pytest-cov==2.2.1',
-    'coverage==4.0.3',
+    'coverage==4.2',
 
     # Fixtures, test helpers
     'factory-boy==2.7.0',
@@ -35,28 +34,28 @@ test_requires = [
 
 install_requires = [
     # General dependencies
-    'django==1.9.5',
+    'django==1.10.3',
 
     # Imap utilities
     'dnspython3==1.12.0',
-    'imapclient==1.0.1',
+    'imapclient==1.0.2',
 
     # For async worker support
-    'celery==3.1.23',
+    'celery==4.0.0',
 
     # i18n/l10n,
     'babel==2.3.4',
     'django-babel==0.5.1',
 
     # For our REST Api
-    'djangorestframework==3.3.3',
-    'PyJWT==1.4.0',
+    'djangorestframework==3.5.3',
+    'PyJWT==1.4.2',
     'requests==2.9.1',
-    'urllib3==1.14',
-    'requests-toolbelt==0.6.0',
+    'urllib3==1.19.1',
+    'requests-toolbelt==0.7.0',
 
     # For proper timezone support.
-    'pytz==2016.4',
+    'pytz==2016.7',
 
     # Charset detection
     'chardet==2.3.0'
@@ -64,13 +63,13 @@ install_requires = [
 
 
 docs_requires = [
-    'sphinx==1.4.1',
+    'sphinx==1.4.8',
     'sphinx_rtd_theme'
 ]
 
 
 postgresql_requires = [
-    'psycopg2==2.6.1',
+    'psycopg2==2.6.2',
 ]
 
 
